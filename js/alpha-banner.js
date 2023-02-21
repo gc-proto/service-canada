a8ae923c6f7d5b6425afa6542091719ea233d1db
Cora Hansen <cora.hansen@hrsdc-rhdcc.gc.ca>
2023-02-21 11:49:22 -0500

    editing text for banner

--- js/alpha-banner.js
+++ js/alpha-banner.js
@@ -1,3 +1,3 @@
 
 var alphaBanner = document.getElementsByTagName('BODY')[0];
-alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top" role="banner"><h2 class="wb-inv">This webpage is in alpha</h2><div><small><span class="alpha-label">Alpha</span>&nbsp;&nbsp; This is an experimental design manual. It is a work in progress.</small></div></section >');
+alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top" role="banner"><h2 class="wb-inv">This webpage is in alpha</h2><div><large><span class="alpha-label">Alpha</span>&nbsp;&nbsp; This is an experimental version of Canada.ca meant for public testing. It is a work in progress.</large></div></section >');
